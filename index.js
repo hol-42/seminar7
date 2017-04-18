e649c1526bfff6cc5b5694f640913e75830b8a78
Juergen Hollfelder <info@hollfelder.eu>
2017-04-18 12:34:14 +0000

    branch commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req,res) {
-    res.send('Hallo erneut Cloud9')
+    res.send('Hallo doch aus Cloud9')
 })
 
 app.get('/qr/:qrstring', function (req, res) {
