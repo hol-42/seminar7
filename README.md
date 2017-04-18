ed42345973b960a5886211953de3efc19fbf4827
Juergen Hollfelder <info@hollfelder.eu>
2017-04-18 10:32:44 +0000

    erst Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Unsere kleine Web App 
+
+Generiert QR Codes als SVG files http://<domain>/qr/<string der qr code werden soll>
---
+++ index.js
@@ -0,0 +1,20 @@
+var qr = require('qr-image')
+var express = require('express')
+
+var app = express()
+
+app.get('/', function (req,res) {
+    res.send('Hallole')
+})
+
+app.get('/qr/:qrstring', function (req, res) {
+    console.log('app get with param', req.param)
+    var code = qr.image(req.params.qrstring, { type: 'svg'})
+    res.type('svg')
+    code.pipe(res)
+})
+
+console.log('port',process.env.PORT)
+console.log('IP',process.env.IP)
+
+app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0")
