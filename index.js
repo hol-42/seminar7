9c93f195ecc8db3956428f9827c3b2561fca6917
Juergen Hollfelder <info@hollfelder.eu>
2017-04-18 11:39:04 +0000

    Schöne Grüsse aus Cloud9

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req,res) {
-    res.send('Hallole')
+    res.send('Schöne Grüsse server.js')
 })
 
 app.get('/qr/:qrstring', function (req, res) {
