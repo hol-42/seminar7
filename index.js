3f6877df52150dbe3e78f7913fc2c6b0418739b5
Juergen Hollfelder <info@hollfelder.eu>
2017-04-18 13:08:29 +0000

    test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req,res) {
-    res.send('Hallo')
+    res.send('Hallo 2')
 })
 
 app.get('/qr/:qrstring', function (req, res) {
