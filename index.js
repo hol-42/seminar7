37d45dfc1824db15785bbfaab280ceeba8266677
Juergen Hollfelder <info@hollfelder.eu>
2017-04-24 04:51:53 +0000

    Hallo sollte aus Cloud9 kommen

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req,res) {
-    res.send('Hallo 3')
+    res.send('Hallo wirklich aus Cloud9 !!!')
 })
 
 app.get('/qr/:qrstring', function (req, res) {
