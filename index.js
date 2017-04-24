fb97a07796ee2aeba2ba534c167ae4c7419f833b
Jürgen Hollfelder <juergen.hollfelder@gmail.com>
2017-04-24 06:48:18 +0200

    Pull Request für Gruss aus Github

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req,res) {
-    res.send('Hallo')
+    res.send('Hallo wirklich aus Github')
 })
 
 app.get('/qr/:qrstring', function (req, res) {
